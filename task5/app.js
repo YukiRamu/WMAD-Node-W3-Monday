64c12b214975f53e74efdfd04bc186e272a21549
Yuki <yukipooh4120@gmail.com>
2021-08-10 02:20:59 -0700

    task5

--- task5/app.js
+++ task5/app.js
@@ -5,3 +5,45 @@ At least one lowercase
 At least one uppercase
 At least one special character from @ # $ % ^ & *
  */
+
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = 6060;
+const bodyParser = require("body-parser");
+const pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const validatePassword = (password) => {
+  if (password.length < 6 || !password.match(pattern)) {
+    console.log("invalid password");
+  } else {
+    console.log("valid password");
+  }
+};
+
+app.post("/", (req, res) => {
+  //To access POST variable use req.body()methods.
+  console.log("password input is ", req.body.password);
+  validatePassword(req.body.password);
+});
+
+app.get("/", (req, res) => {
+  //read html
+  fs.readFile("./index.html", (error, data) => {
+    if (error) {
+      throw error;
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Application is running  on the port ${port}`);
+});
